949aee6de2129f724eb43783b9d4dc2c2bd52be0
franncesco <francesco.coriolano@gmail.com>
2021-11-25 17:27:02 +0000

    merging

--- src/components/SongCardHeader/index.js
+++ src/components/SongCardHeader/index.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "./style.css";
+import image from "../../images/Better-Call-Saul.jpeg";
 function SongCardHeader() {
   return (
     <div className="songCardHeaderMain">
       <div className="iconArrow">
         <span class="material-icons">arrow_back</span>
       </div>
-      <div className="song-image">
-        <image></image>
-      </div>
-      <div className="song-details">
-        <h2>song title</h2>
-        <h3>artist name</h3>
-      </div>
-      <div className="audio-control">
-        <audio></audio>
+      <div className="song-card">
+        <div className="song-image-container">
+          <img src={image} alt="Image" className="image-song" />
+        </div>
+        <div className="song-details">
+          <h2>song title</h2>
+          <h3>artist name</h3>
+        </div>
+        <div className="audio-control">
+          <audio></audio>
+        </div>
       </div>
     </div>
   );
