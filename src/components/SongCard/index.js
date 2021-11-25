287217e3bb4550f9158d5a644a1b45caf6bd5c74
SusanaAlvarezZuluaga <susialvarezzuluaga@hotmail.com>
2021-11-25 12:22:59 +0000

    general structure

---
+++ src/components/SongCard/index.js
@@ -0,0 +1,11 @@
+import SongCardHeader from '../SongCardHeader';
+import SongCardLyrics from '../SongCardLyrics';
+function SongCard() {
+  return (
+    <div>
+      <SongCardHeader />
+      <SongCardLyrics />
+    </div>
+  );
+}
+export default SongCard;
