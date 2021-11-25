090c133d6a31dd54327bb83fc26fc2a02d61bedb
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-11-25 15:34:54 +0000

    commit with title and artist for michaela

--- src/components/SongCard/index.js
+++ src/components/SongCard/index.js
@@ -1,11 +1,57 @@
 import SongCardHeader from '../SongCardHeader';
 import SongCardLyrics from '../SongCardLyrics';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+
 function SongCard() {
-  return (
-    <div>
-      <SongCardHeader />
-      <SongCardLyrics />
-    </div>
-  );
-}
+
+  console.log('hola');
+  let songId = 3135556
+  
+  let [songInfo, setSongInfo] = useState([]);
+
+  
+  // };
+  function getAllSongInfo() {
+    // axios.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${songId}`)
+      axios.get(`https://api.deezer.com/track/${songId}`)
+      .then(resp => {
+       // console.log(resp.data);
+        setSongInfo(resp.data);
+
+      })
+      .catch(err => {
+        // Handle Error Here
+        console.error(err);
+      });
+  }
+  
+  // getAllSongInfo();
+
+  useEffect(() => {
+    console.log(`mount`)
+    getAllSongInfo()
+  }, []);
+
+  // let artistName = songInfo.contributors[0].name;
+  // console.log(artistName);
+ // console.log(songInfo.contributors[0].name);
+  //console.log(songInfo.contributors[0].name);
+  
+  console.log(songInfo.title);
+
+return (
+  <div>
+    <SongCardHeader  />
+    <SongCardLyrics title= {songInfo.title} artist={songInfo.contributors[0].name} />
+    
+  </div>
+);
+
+
+};
+
+
 export default SongCard;
--- src/components/SongCardLyrics/index.js
+++ src/components/SongCardLyrics/index.js
@@ -1,4 +1,8 @@
-function SongCardLyrics() {
+function SongCardLyrics({ title, artist }) {
+  
+  console.log(title, artist)
+
+
   return <div>song lyrics </div>;
 }
 export default SongCardLyrics;
