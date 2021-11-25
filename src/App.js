8237e65697837dec2b929fa2cb0733eb75879011
franncesco <francesco.coriolano@gmail.com>
2021-11-25 16:32:15 +0000

    time to push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchPage from "./components/SearchPage";
 import SongCard from "./components/SongCard";
-
+import SongCardHeader from "./components/SongCardHeader";
+import "./style.css";
 function App() {
   return (
+    <div>
+      <SongCardHeader />
+    </div>
+    /*
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<SearchPage />} />
         <Route path="/songs/:id" element={<SongCard />} />
       </Routes>
     </BrowserRouter>
+    */
   );
 }
 
--- src/components/SongCardHeader/index.js
+++ src/components/SongCardHeader/index.js
@@ -1,4 +1,22 @@
+import React from "react";
+import "./style.css";
 function SongCardHeader() {
-  return <div>song card header</div>;
+  return (
+    <div className="songCardHeaderMain">
+      <div className="iconArrow">
+        <span class="material-icons">arrow_back</span>
+      </div>
+      <div className="song-image">
+        <image></image>
+      </div>
+      <div className="song-details">
+        <h2>song title</h2>
+        <h3>artist name</h3>
+      </div>
+      <div className="audio-control">
+        <audio></audio>
+      </div>
+    </div>
+  );
 }
 export default SongCardHeader;
