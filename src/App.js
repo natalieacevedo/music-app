c01c5de3a20a0f6f9edc1566fd1aa463ec9492e2
SusanaAlvarezZuluaga <susialvarezzuluaga@hotmail.com>
2021-11-26 12:31:12 +0000

    final version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SearchPage from "./components/SearchPage/";
-import SongCard from "./components/SongCard";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SearchPage from './components/SearchPage/';
+import SongCard from './components/SongCard';
 
-import "./style.css";
+import './style.css';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path='/' element={<SearchPage />} />
-        <Route path='/songs/:id' element={<SongCard />} />
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/songs/:id" element={<SongCard />} />
       </Routes>
     </BrowserRouter>
   );
